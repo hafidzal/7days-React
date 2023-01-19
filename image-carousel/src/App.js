fa317213e0ecb555fb1089327a71fa1b785ef2fd
hafidzal <hafidzalimaulana@gmail.com>
2023-01-20 01:49:28 +0700

    Init and add first project

---
+++ image-carousel/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Image Carousel</h1>
+      <div className="slide">
+          <img src='https://images.pexels.com/photos/3836292/pexels-photo-3836292.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' alt="images" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
