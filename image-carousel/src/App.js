df62565a02a6fbb544e4471f5f33bec7c0b8b384
hafidzal <hafidzalimaulana@gmail.com>
2023-01-21 23:20:43 +0700

    feature: add slider and mapping image

--- image-carousel/src/App.js
+++ image-carousel/src/App.js
@@ -1,12 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+
+const images = [
+  "https://images.pexels.com/photos/3836292/pexels-photo-3836292.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  "https://images.pexels.com/photos/2792157/pexels-photo-2792157.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  "https://images.pexels.com/photos/1166209/pexels-photo-1166209.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  "https://images.pexels.com/photos/1661179/pexels-photo-1661179.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+];
 
 function App() {
+  const [current, setCurrent] = useState(0);
+  function nextSlide() {
+    setCurrent(current === images.length - 1 ? 0 : current + 1);
+  }
+
+  function prevSlide() {
+    setCurrent(current === 0 ? images.length - 1 : current - 1)
+  }
   return (
     <div className="App">
       <h1>Image Carousel</h1>
-      <div className="slide">
-          <img src='https://images.pexels.com/photos/3836292/pexels-photo-3836292.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' alt="images" />
+      <div className="slider">
+        <button className="left-arrow" onClick={prevSlide}>
+          &larr;
+        </button>
+          {
+            images.map( (img, i) => 
+              current === i && (<div key={img}>
+                <img src={img} alt="images"/>
+              </div>)
+            ) 
+          }
+
+        <button className="right-arrow" onClick={nextSlide}>
+          &rarr;
+        </button>
       </div>
     </div>
   );
